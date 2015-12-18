bfde3d5bdfc352a244e7f8389e5b567c60f00649
przemo2174 <przemo217@hotmail.com>
2015-12-18 16:41:51 +0100

    first commit

---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import json
+
+
+class ClientConnection:
+    def __init__(self, client_ip_address='127.0.0.1', client_port=8080):
+        self.ip_address = client_ip_address
+        self.port = client_port
+        self.country = ''
+        self.type = 'text'
+        self.json = {}
+
+    def generate_request_json(self, country, tag=None, get_flag=False, check_flag=None):
+        self.country = country
+        request = 'country(%s)' % self.country
+        if tag is not None:
+            request += ';tag(%s)' % tag
+        if get_flag:
+            request += ';getflag'
+            self.type = 'image'
+        if check_flag is not None:
+            request = 'checkflag(%s)' % check_flag
+
+        self.json = {'address': self.ip_address, 'port': self.port, 'type': self.type, 'content': request}
+
+    def send_json(self, server_ip_address, server_port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((server_ip_address, server_port))
+        mssg = json.dumps(self.json)
+        sock.send(mssg)
+        while True:
+            data = sock.recv(512)
+            if len(data) < 1:
+                break
+            print data
+        sock.close()
+
+    def print_json(self):
+        print json.dumps(self.json)
+
+conn = ClientConnection()
+conn.generate_request_json('Poland', get_flag=True)
+conn.print_json()
+
